e790d1cb7403c88701d9bebaf74ca7cb4c532f67
Vanne10 <vanessa.burrola.soto@gmail.com>
2022-05-11 11:10:49 -0600

    Nuecleo del servidor

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
-var express = require('express');
+//Require packages and set the port
+const express = require('express');
+const port = 3002;
+
+//Para permitir manejo de POST y PUT
+const bodyParser = require('body-parser');
+const routes = require('./routes/routes')
+const app = express();
+
+// Usar Node.js body parsing middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+
+routes(app);
+
+// Iniciar el servidor
+const server = app.listen(port, (error) => {
+    if (error) return console.log(`Error: ${error}`);
+
+    console.log(`El servidor escucha en el puerto ${server.address().port}`);
+});
+
+/*var express = require('express');
 var app = express();
 
 app.get('/hola', function (req, res) {
@@ -11,4 +35,4 @@ app.get('/adios', function (req, res) {
 
 app.listen(3000, function () {
     console.log('Aplicación ejemplo, escuchando el puerto 3000!');
-});
+});*/
