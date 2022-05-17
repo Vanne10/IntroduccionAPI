961ca2af20e7e636ea159ac48d6a53b97c82e326
Vanne10 <vanessa.burrola.soto@gmail.com>
2022-05-17 07:28:58 -0600

    Conexión a SQL Server

--- data/config.js
+++ data/config.js
@@ -1,4 +1,39 @@
-const mysql = require('mysql');
+const sql = require('mssql');
+
+const configS = {
+    user: 'vane',
+    password: 'password',
+    server: 'localhost',
+    database: 'api',
+    options: {
+        "instanceName": "MSSQLSERVER",
+        trustServerCertificate: true
+    },
+};
+const pool = {
+    query: function (query, callback) {
+        sql.connect(configS).then((pool) => {
+            return pool.query(query);
+        }).then(result => {
+            callback(null, result);
+        }).catch(err => {
+            callback(err, null);
+        });
+    }
+}
+module.exports = pool;
+/*const pool = {
+    query: function (query, callback) {
+        sql.connect(configS).then((pool) => {
+            return pool.query(query);
+        }).then(result => {
+            callback(null, result);
+        }).catch(err => {
+            callback(err, null);
+        });
+    }
+}*/
+/*const mysql = require('mysql');
 
 //Set database connection credentials
 const config = {
@@ -12,4 +47,4 @@ const config = {
 const pool = mysql.createPool(config);
 //nuevo commit
 //Export the pool
-module.exports = pool;
+module.exports = pool;*/
